d44651c682b7ad9478979dd800ea4fab9312fe23
palesl <palesl@essex.ac.uk>
2017-02-09 11:09:24 +0000

    update docs

--- R/pdf_corpus.R
+++ R/pdf_corpus.R
@@ -1,6 +1,19 @@
 #' Pdf documents to a corpus
 #'
 #' This function reads pdf files into R, and creates a corpus for text analysis.
+#'
+#' IMPORTANT: This function requires the installation of additional pdf extraction software.
+#'
+#' The extraction engine comes courtesy of http://www.foolabs.com/xpdf/download.html (open source)
+#'
+#' Download the appropriate file for your operating system
+#'
+#' xpdfbin-mac-3.04.tar.gz for MacOS or OSX
+#' xpdfbin-win-3.04.zip for Windows
+#' xpdfbin-linux-3.04.tar.gz for Linux
+#'
+#' Extract the folders and install according to the instructions within the INSTALL file.
+#'
 #' @param path A directory address to a folder containing one or more .pdf documents.
 #' @keywords pdf Corpus
 #' @export
