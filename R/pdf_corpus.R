27a27515970c098845abeb69a5c4d52c1693daa2
palesl <palesl@essex.ac.uk>
2017-02-09 10:35:33 +0000

    cleaning up names

--- R/pdf_corpus.R
+++ R/pdf_corpus.R
@@ -1,8 +1,8 @@
 #' Pdf documents to a corpus
 #'
 #' This function reads pdf files into R, and creates a corpus for text analysis.
-#' @param path A directory address to a folder containing one or more .pdf documents.  
-#' @keywords pdf Corpus 
+#' @param path A directory address to a folder containing one or more .pdf documents.
+#' @keywords pdf Corpus
 #' @export
 #' @examples
 #' pdf_corpus(path = '~/User/folder/')
@@ -10,16 +10,19 @@
 pdf_corpus <- function(path){
   library(tm)
   files <- list.files(path = path, pattern = "pdf$")
-  
+
   files <- paste(path, '/', files, sep='' )
-  
+
   files <- gsub('//','/', files)
-  
+
   Rpdf <- readPDF(control = list(text = "-layout"))
-  
-  corpus <- Corpus(URISource(files), 
+
+  corpus <- Corpus(URISource(files),
                      readerControl = list(reader = Rpdf))
-  
+
+  names(corpus) <- gsub('.pdf','', names(corpus))
+
+
   return(corpus)
-  
+
 }
