80f4a1cb74660ff23171253633ee9f0192798dbb
palesl <palesl@essex.ac.uk>
2017-02-11 14:22:06 +0000

    re document

--- R/pdf_corpus.R
+++ R/pdf_corpus.R
@@ -1,7 +1,11 @@
 #' Pdf documents to a corpus
 #'
 #' This function reads pdf files into R, and creates a corpus for text analysis.
-
+#' @param path A directory address to a folder containing one or more .pdf documents.
+#' @keywords pdf Corpus
+#' @export
+#' @examples
+#' pdf_corpus(path = '~/User/folder/')
 
 pdf_corpus <- function(path){
   library(tm)
